4b6ef05a9a6154767df287901397a7b5e11e2072
Ayoub Jdair <48654991+AyoubJ324@users.noreply.github.com>
2021-05-04 17:10:22 +0100

    turned off error blocking

--- notifications.php
+++ notifications.php
@@ -12,8 +12,6 @@
     <body class="notificationPage">
 
        <?php
-        error_reporting(0);
-        ini_set('display_errors', 0);
         require "navbar.php";
         require "notifications_utils.php";
         require "database.php";
